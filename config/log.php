4bfcdc870030e7a308cc18a94db7f23c0b225f5c
Darren Edale <darren@equituk.net>
2023-12-01 16:31:48 +0000

    Fix incorrect log file name in sample config.

--- config/log.php
+++ config/log.php
@@ -7,7 +7,7 @@ use Bead\Logging\FileLogger;
 return [
     "driver" => "file",
     "file" => [
-        "path" => "data/logs/dead-drop.log",
+        "path" => "data/logs/dev-tools.log",
         "flags" => FileLogger::FlagAppend
     ],
 ];
